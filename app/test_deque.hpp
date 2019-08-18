ddb903a2ecefd164ec1fba732ea13182f328271b
ncy94 <sagatncy@gmail.com>
2019-08-18 16:49:09 -0700

    created deque class

---
+++ app/test_deque.hpp
@@ -0,0 +1,10 @@
+//
+// Created by NCY on 2019-08-18.
+//
+
+#ifndef STLCONTAINER_TEST_DEQUE_HPP
+#define STLCONTAINER_TEST_DEQUE_HPP
+
+#include "deque.hpp"
+
+#endif //STLCONTAINER_TEST_DEQUE_HPP
---
+++ include/deque.hpp
@@ -0,0 +1,16 @@
+//
+// Created by NCY on 2019-08-18.
+//
+
+#ifndef STLCONTAINER_DEQUE_HPP
+#define STLCONTAINER_DEQUE_HPP
+
+namespace sc::regular{
+
+    template <class T>
+    class deque{
+
+    };
+}
+
+#endif //STLCONTAINER_DEQUE_HPP
