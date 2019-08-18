64654773dc38e74569b4e8e3a1a8cee7eff61de7
ncy94 <sagatncy@gmail.com>
2019-08-17 17:20:35 -0700

    added copy/move constructor and assignment operator for node base

--- utils/node_base.hpp
+++ utils/node_base.hpp
@@ -5,6 +5,8 @@
 #ifndef STLCONTAINER_NODE_BASE_HPP
 #define STLCONTAINER_NODE_BASE_HPP
 
+#include <algorithm>
+
 namespace sc::utils{
 
     // single direction node base
@@ -12,6 +14,28 @@ namespace sc::utils{
     class node_base{
     public:
         node_base(): val_(0), next_(nullptr){}
+
+        node_base(const node_base& other): val_(other.val_), next_(other.next_) {}
+        node_base(node_base&& other): val_(std::move(other.val_)), next_(std::move(other.next_)){
+            val_ = other.val_;
+            other.val_ = 0;
+            next_ = other.next_;
+            other.next_ = nullptr;
+        }
+
+        node_base&operator=(const node_base& other){
+            val_ = other.val_;
+            next_ = other.next_;
+            return *this;
+        }
+
+        node_base&operator=(node_base&& other) noexcept {
+            val_ = other.val_;
+            other.val_ = 0;
+            next_ = other.next_;
+            other.next_ = nullptr;
+            return *this;
+        }
 
         node_base(T val): val_(val), next_(nullptr){}
 
